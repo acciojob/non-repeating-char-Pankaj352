0e85091519b082740785aa7067cf5f99827788bb
Pankaj Maurya <100466858+Pankaj352@users.noreply.github.com>
2025-02-06 13:40:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 function firstNonRepeatedChar(str) {
-    const countCharObj = {};
-    for (let i=0;i<str.length;i++) {
-		let char=str[i];
-        countCharObj[char]=(countCharObj[char]|| 0) + 1;
+    const charCount = {};
+
+    for (let char of str) {
+        charCount[char] = (charCount[char] || 0) + 1;
     }
-    for (let i=0;i<str.length;i++) {
-		let char=str[i];
-        if (countCharObj[char]===1) {
+
+    for (let char of str) {
+        if (charCount[char] === 1) {
             return char;
         }
     }
+
     return null;
 }
 
-// const input = prompt("Enter a string");
+// Example usage
+const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input));
